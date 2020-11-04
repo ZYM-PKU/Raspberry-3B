934dec70596f92d3eebf41178021df7ea83bb3d3
ZYM-PKU <82540602@qq.com>
2020-11-04 11:33:57 +0800

    up

---
+++ 4/1.py
@@ -0,0 +1,60 @@
+import time
+import spidev as SPI
+import SSD1306
+from PIL import Image,ImageDraw,ImageFont # 调用相关库文件
+from datetime import datetime
+
+RST = 19
+DC = 16
+bus = 0
+device = 0 # 树莓派管脚配置
+disp = SSD1306.SSD1306(rst=RST,dc=DC,spi=SPI.SpiDev(bus,device))
+
+disp.begin()
+disp.clear()
+
+
+def gettime():
+    dt=datetime.now()
+    hour='0'+str(dt.hour) if len(str(dt.hour))==1 else str(dt.hour)
+    minute='0'+str(dt.minute) if len(str(dt.minute))==1 else str(dt.minute)
+    second='0'+str(dt.second) if len(str(dt.second))==1 else str(dt.second)
+    timestr=hour+':'+minute+':'+second
+
+    return timestr
+
+
+def disp1():
+    '''显示helloworld'''
+    font = ImageFont.load_default()
+    image = Image.new('RGB',(disp.width,disp.height),'black').convert('1')
+    draw = ImageDraw.Draw(image)
+    draw.bitmap((0,0), logo, fill=1)
+    draw.text((x,top), 'Hello World!', font=font, fill=255)
+    disp.image(image)
+    disp.display() # 显示图片
+
+
+def disp2():
+    '''显示时钟'''
+
+
+    time=gettime()
+
+    logo=Image.open('p128.png').resize((32,32),Image.ANTIALIAS).convert('1')#logo
+    img = Image.new('1',(disp.width,disp.height),'black')#final_img
+    img.paste(logo, (0, 0, logo.size[0], logo.size[1]))
+
+    font = ImageFont.load_default()
+    draw = ImageDraw.Draw(image)
+    draw.bitmap((0,0), logo, fill=1)
+    draw.text((x,top), time, font=font, fill=255)
+
+
+    disp.clear()
+    disp.image(img)
+    disp.display()
+
+
+if __name__ == "__main__":
+    disp1()
---
+++ 4/3.py
@@ -0,0 +1,75 @@
+import time
+import spidev as SPI
+import SSD1306
+from PIL import Image,ImageDraw,ImageFont # 调用相关库文件
+import matplotlib.pyplot as plt
+from sklearn import datasets, svm, metrics
+import random
+
+import RPi.GPIO as GPIO
+KEY = 20
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(KEY, GPIO.IN,GPIO.PUD_UP)
+
+RST = 19
+DC = 16
+bus = 0
+device = 0 # 树莓派管脚配置
+disp = SSD1306.SSD1306(rst=RST,dc=DC,spi=SPI.SpiDev(bus,device))
+
+disp.begin()
+disp.clear()
+
+images_and_predictions=[]
+
+
+def train():
+    global images_and_predictions
+
+    digits = datasets.load_digits()
+
+    # To apply a classifier on this data, we need to flatten the image, to
+    # turn the data in a (samples, feature) matrix:
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+    # Create a classifier: a support vector classifier
+    classifier = svm.SVC(gamma=0.001)
+    # We learn the digits on the first half of the digits
+    classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
+    # Now predict the value of the digit on the second half:
+    expected = digits.target[n_samples // 2:]
+    predicted = classifier.predict(data[n_samples // 2:])
+
+
+    images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
+
+
+
+for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+    plt.subplot(2, 4, index + 5)
+    plt.axis('off')
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title('Prediction: %i' % prediction)
+plt.show()
+
+def showimg():
+
+    kk,pre=random.choice(images_and_predictions)
+    digit = Image.fromarray((kk*8).astype(np.uint8), mode='L').resize((48,48)).convert('1')
+
+    img = Image.new('1',(disp.width,disp.height),'black')
+    img.paste(digit, (0, 16, digit.size[0], digit.size[1]+16))
+    disp.clear()
+    disp.image(img)
+    disp.display()
+
+    print(f"predictions:  {pre}")
+
+
+def main():
+    images_and_predictions=train()
+    add_event_detect(channel, GPIO.RISING, callback=showimg, bouncetime=200)
+
+
+
+
