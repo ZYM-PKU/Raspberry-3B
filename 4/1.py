ec621297dab468d35c2da7bd03315cdb28f02638
ZYM-PKU <82540602@qq.com>
2020-11-05 22:58:03 +0800

    u

--- 4/1.py
+++ 4/1.py
@@ -1,7 +1,8 @@
-import time,os
+import time
+import os
 import spidev as SPI
 import SSD1306
-from PIL import Image,ImageDraw,ImageFont # 调用相关库文件
+from PIL import Image, ImageDraw, ImageFont  # 调用相关库文件
 from datetime import datetime
 
 
@@ -9,19 +10,19 @@ PATH = os.path.dirname(__file__)
 RST = 19
 DC = 16
 bus = 0
-device = 0 # 树莓派管脚配置
-disp = SSD1306.SSD1306(rst=RST,dc=DC,spi=SPI.SpiDev(bus,device))
+device = 0  # 树莓派管脚配置
+disp = SSD1306.SSD1306(rst=RST, dc=DC, spi=SPI.SpiDev(bus, device))
 
 disp.begin()
 disp.clear()
 
 
 def gettime():
-    dt=datetime.now()
-    hour='0'+str(dt.hour) if len(str(dt.hour))==1 else str(dt.hour)
-    minute='0'+str(dt.minute) if len(str(dt.minute))==1 else str(dt.minute)
-    second='0'+str(dt.second) if len(str(dt.second))==1 else str(dt.second)
-    timestr=hour+':'+minute+':'+second
+    dt = datetime.now()
+    hour = '0'+str(dt.hour) if len(str(dt.hour)) == 1 else str(dt.hour)
+    minute = '0'+str(dt.minute) if len(str(dt.minute)) == 1 else str(dt.minute)
+    second = '0'+str(dt.second) if len(str(dt.second)) == 1 else str(dt.second)
+    timestr = hour+':'+minute+':'+second
 
     return timestr
 
@@ -29,46 +30,45 @@ def gettime():
 def disp1():
     '''显示helloworld'''
 
-    font = ImageFont.truetype("comicsansms.ttf",20)
-    image = Image.new('RGB',(disp.width,disp.height),'black').convert('1')
+    font = ImageFont.truetype("comicsansms.ttf", 20)
+    image = Image.new('RGB', (disp.width, disp.height), 'black').convert('1')
     draw = ImageDraw.Draw(image)
-    draw.bitmap((0,0), image, fill=1)
-    draw.text((10,20), 'Hello World!', font=font, fill=255)
+    draw.bitmap((0, 0), image, fill=1)
+    draw.text((10, 20), 'Hello World!', font=font, fill=255)
     disp.image(image)
-    disp.display() # 显示图片
+    disp.display()  # 显示图片
 
 
 def disp2():
     '''显示时钟'''
     while True:
 
+        nowtime = gettime()
 
-        nowtime=gettime()
-
-        logo=Image.open(os.path.join(PATH,'p128.png')).resize((32,32),Image.ANTIALIAS).convert('1')#logo
-        img = Image.new('1',(disp.width,disp.height),'black')#final_img
+        logo = Image.open(os.path.join(PATH, 'p128.png')).resize(
+            (32, 32), Image.ANTIALIAS).convert('1')  # logo
+        img = Image.new('1', (disp.width, disp.height), 'black')  # final_img
         img.paste(logo, (0, 0, logo.size[0], logo.size[1]))
 
-        font = ImageFont.truetype("comicsansms.ttf",13)
+        font = ImageFont.truetype("comicsansms.ttf", 13)
         draw = ImageDraw.Draw(img)
-        draw.bitmap((0,0), img, fill=1)
-        draw.text((64,0), nowtime, font=font, fill=255)
-        draw.text((32,15), "Count down of ", font=font, fill=255)
-        draw.text((50,30), "mid-term:", font=font, fill=255)
+        draw.bitmap((0, 0), img, fill=1)
+        draw.text((64, 0), nowtime, font=font, fill=255)
+        draw.text((32, 15), "Count down of ", font=font, fill=255)
+        draw.text((50, 30), "mid-term:", font=font, fill=255)
 
-        tardate=datetime(2020,11,9)
-        nowdate=datetime.now()
-        delta=tardate-nowdate
-        days=delta.days
-        seconds=delta.seconds
-        hours=seconds//3600
-        seconds=seconds%3600
-        minutes=seconds//60
-        seconds=seconds%60
+        tardate = datetime(2020, 11, 9)
+        nowdate = datetime.now()
+        delta = tardate-nowdate
+        days = delta.days
+        seconds = delta.seconds
+        hours = seconds//3600
+        seconds = seconds % 3600
+        minutes = seconds//60
+        seconds = seconds % 60
 
-        draw.text((0,45), f"{days}d  {hours}hour {minutes}min {seconds}s", font=font, fill=255)
-
-
+        draw.text((0, 45), f"{days}d  {hours}hour {minutes}min {seconds}s", 
+        font=font, fill=255)
 
         disp.clear()
         disp.image(img)
@@ -78,4 +78,3 @@ def disp2():
 
 if __name__ == "__main__":
     disp2()
-
--- 4/2.py
+++ 4/2.py
@@ -11,7 +11,7 @@ digits = datasets.load_digits()
 # the dataset.
 images_and_labels = list(zip(digits.images, digits.target))
 
-for index, (image, label) in enumerate(images_and_labels[:4]):
+for index, (image, label) in enumerate(images_and_labels[:8]):
     plt.subplot(2, 4, index + 1)
     plt.axis('off')
     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
@@ -37,8 +37,8 @@ print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
 images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
 
-for index, (image, prediction) in enumerate(images_and_predictions[:4]):
-    plt.subplot(2, 4, index + 5)
+for index, (image, prediction) in enumerate(images_and_predictions[:8]):
+    plt.subplot(2, 4, index + 1)
     plt.axis('off')
     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     plt.title('Prediction: %i' % prediction)
