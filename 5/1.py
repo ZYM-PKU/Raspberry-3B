10aa4fd508014ea249874131834fdaa6fa5ee5f0
ZYM-PKU <61368442+ZYM-PKU@users.noreply.github.com>
2020-11-11 14:16:21 +0800

    Add files via upload

--- 5/1.py
+++ 5/1.py
@@ -1,16 +1,33 @@
 import smbus
 import time # 包含相关库文件
+from datetime import datetime
 address = 0x68
 register = 0x00
 bus = smbus.SMBus(1) # 初始化i2c Bus
 
 # FixTime 定义为2019 年6 月12 日18 时
-FixTime = [0x00,0x00,0x13,0x03,0x11,0x11,0x10]
+FixTime = [0x00,0x00,0x13,0x03,0x11,0x11,0x20]
+
+def gettime():
+    dt = datetime.now()
+    return dt.hour,dt.minute,dt.second
+
 
 def ds3231SetTime():
+    h,m,s=gettime()
+    h,m,s=16*(h//10)+h%10,16*(m//10)+m%10,16*(s//10)+s%10
+    FixTime = [s,m,h,0x03,0x11,0x11,0x20]
     bus.write_i2c_block_data(address,register,FixTime)
 def ds3231ReadTime():
-    return bus.read_i2c_block_data(address,register,7);
+    datas= bus.read_i2c_block_data(address,register,7)
+    datas=[(data//16)*10+data%16  for data in datas]
+    return datas
 
 ds3231SetTime() # 设置时间
-print(ds3231ReadTime()) # 读出时间   
+
+while True:
+    t=ds3231ReadTime()
+    print(t) # 读出时间
+    print(f"Time: {t[6]}year {t[5]}month {t[4]}day  {t[2]}h {t[1]}m {t[0]}s")
+    time.sleep(1)
+
